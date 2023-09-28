5c6c5d3ce3ff895f6d14ccc6eb58ebfd11d19359
Peter Nguyen <42896783+petern2599@users.noreply.github.com>
2023-09-28 14:56:46 -0500

    added JSON logger and modified ui

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from components.deck_factory import DeckFactory
 from components.vocab_card import VocabCard
+from components.json_logger import JSONLogger
 
 class VocabularyPracticeApp(QMainWindow):
     def __init__(self):
@@ -10,8 +11,9 @@ class VocabularyPracticeApp(QMainWindow):
         self.show()
         self.ui_setup()
         self.deck_factory = DeckFactory()
-        self.deck_factory.set_amount(10)
-        self.practice_mode = ""
+        self.set_default_settings()
+
+        self.json_logger = JSONLogger()
 
     def ui_setup(self):
         self.actionAdd_Spreadsheet.triggered.connect(self.add_spreadsheet_pressed)
@@ -22,18 +24,43 @@ class VocabularyPracticeApp(QMainWindow):
         self.incorrect_button.clicked.connect(self.incorrect_button_pressed)
         self.show_button.clicked.connect(self.show_button_pressed)
 
+    def set_default_settings(self):
+        self.deck_factory.set_amount(10)
+        self.practice_mode = "Practice Vocab"
+
     def add_spreadsheet_pressed(self):
-        spreadsheet_path = QFileDialog.getOpenFileName(self,'Select file','')
-        self.deck_factory.read_spreadsheet(spreadsheet_path[0])
+        try:
+            spreadsheet_path = QFileDialog.getOpenFileName(self,'Select spreadsheet file','',"csv(*.csv)")
+            self.deck_factory.read_spreadsheet(spreadsheet_path[0])
+
+        except FileNotFoundError:
+            message = QMessageBox()
+            message.setText("No file was selected...")
+            message.setWindowTitle('Message')
+            message.exec_()
+        
+    def disable_buttons(self):
+        self.show_button.setEnabled(False)
+        self.correct_button.setEnabled(False)
+        self.incorrect_button.setEnabled(False)
+
+    def enable_buttons(self):
+        self.show_button.setEnabled(True)
+        self.correct_button.setEnabled(True)
+        self.incorrect_button.setEnabled(True)
 
     def start_pressed(self):
-        self.vocab_deck = self.deck_factory.create_vocab_deck()
-        self.deck_length = len(self.vocab_deck)
-        self.vocab_deck.reverse()
-        self.counter = 0
-        self.correct = 0
-        self.incorrect = 0
-        self.display_card()
+        if len(self.deck_factory.vocab_df) == 0:
+                self.disable_buttons()
+        else:
+            self.enable_buttons()
+            self.vocab_deck = self.deck_factory.create_vocab_deck()
+            self.deck_length = len(self.vocab_deck)
+            self.vocab_deck.reverse()
+            self.counter = 0
+            self.correct = 0
+            self.incorrect = 0
+            self.display_card()
 
     def set_amount_pressed(self):
         amount,ok = QInputDialog.getText(self, 'Set Amount', 'Type number of vocab cards to generate for deck:')
@@ -102,8 +129,9 @@ class VocabularyPracticeApp(QMainWindow):
         if len(self.vocab_deck) != 0:
             self.display_card()
         else:
+            self.create_log()
             message = QMessageBox()
-            message.setText("No more cards in deck")
+            message.setText("No more cards in deck, logging stats...")
             message.setWindowTitle('Message')
             message.exec_()
 
@@ -112,8 +140,9 @@ class VocabularyPracticeApp(QMainWindow):
         if len(self.vocab_deck) != 0:
             self.display_card()
         else:
+            self.create_log()
             message = QMessageBox()
-            message.setText("No more cards in deck")
+            message.setText("No more cards in deck, logging stats...")
             message.setWindowTitle('Message')
             message.exec_()
 
@@ -126,6 +155,9 @@ class VocabularyPracticeApp(QMainWindow):
             self.show_button_toggle = 0
             self.set_show_button_text()
             self.switch_display_card()
+
+    def create_log(self):
+        self.json_logger.append_daily_stats(self.correct,self.incorrect)
 
 def main():
     app = QApplication([])
---
+++ src/components/json_logger.py
@@ -0,0 +1,64 @@
+import json
+import os
+from datetime import date,timedelta
+
+class JSONLogger():
+    def __init__(self):
+        current_dir = os.getcwd()
+        self.log_path = current_dir + "\log"
+        self.check_log_directory_exists()
+        year_path = self.check_current_year_log_folder_exists()
+        json_path = self.check_current_month_log_folder_exists(year_path)
+
+        
+    
+    def check_log_directory_exists(self):
+        if os.path.exists(self.log_path):
+            pass
+        else:
+            print("Creating Log directory at {}...".format(self.log_path))
+            os.mkdir(self.log_path)
+
+    def check_current_year_log_folder_exists(self):
+        year = date.today().strftime("%Y")
+        new_path = self.log_path + "\{}".format(year)
+        if os.path.exists(new_path):
+            return new_path
+        else:
+            print("Creating Log folder for this year ({})...".format(year))
+            os.mkdir(new_path)
+            return new_path
+
+    def check_current_month_log_folder_exists(self,path):
+        month_year = date.today().strftime("%m-%y")
+        new_path = path + "\{}".format(month_year)
+        if os.path.exists(new_path):
+            return new_path
+        else:
+            print("Creating Log file for this month ({})...".format(month_year))
+            day = date.today().strftime("%m-%d-%y")
+            data = {"{}".format(day):{"correct":0, "incorrect":0}}
+            with open(new_path, 'w') as file:
+                json.dump(data,file,indent=4)
+            
+            return new_path
+            
+    def append_daily_stats(self,correct,incorrect):
+        day = date.today().strftime("%m-%d-%y")
+        self.check_log_directory_exists()
+        year_path = self.check_current_year_log_folder_exists()
+        json_path = self.check_current_month_log_folder_exists(year_path)
+
+        with open(json_path,'r+') as file:
+            file_data = json.load(file)
+            if day in file_data:
+                correct_stat = file_data[day]["correct"]
+                incorrect_stat = file_data[day]["incorrect"]
+                file_data[day]= {"correct":correct_stat + correct, "incorrect":incorrect_stat+incorrect}
+                file.seek(0)
+                json.dump(file_data,file,indent=4)
+            else:
+                print("Adding new entry in log file...")
+                file_data[day] = {"correct":correct, "incorrect":incorrect}
+                file.seek(0)
+                json.dump(file_data,file,indent=4)
